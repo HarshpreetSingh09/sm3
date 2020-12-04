9fc51377bae82db327d52dad7aa141bdd5b7b570
HarshpreetSingh09 <38800823+HarshpreetSingh09@users.noreply.github.com>
2020-12-04 10:55:54 +0530

    Update MembersScraper.py

--- MembersScraper.py
+++ MembersScraper.py
@@ -3,9 +3,9 @@ from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty
 import csv
  
-api_id = 2473030
-api_hash = '0b7b0cc0295a28bf9f032fa2f23c64ce'
-phone = '+13215223978'
+api_id = 2463406
+api_hash = 'bbdc2693d8eb33d0b94be7626a0221fe'
+phone = '+15124889048'
 client = TelegramClient(phone, api_id, api_hash)
  
 client.connect()
